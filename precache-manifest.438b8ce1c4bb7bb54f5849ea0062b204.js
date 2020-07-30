c28fbe930fc7ed4b17fc0c2418d92175a0b20136
charithcshetty <charithcshetty@gmail.com>
2020-07-30 12:26:36 +0530

    Updates

---
+++ precache-manifest.438b8ce1c4bb7bb54f5849ea0062b204.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee511c674ec7f7b54f7905d795ee5a1",
+    "url": "/React-Todo-List-App/index.html"
+  },
+  {
+    "revision": "1531b13b85d03460e93c",
+    "url": "/React-Todo-List-App/static/css/main.628cf573.chunk.css"
+  },
+  {
+    "revision": "7d020f730ee3a581ae1f",
+    "url": "/React-Todo-List-App/static/js/2.56b9b8a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todo-List-App/static/js/2.56b9b8a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1531b13b85d03460e93c",
+    "url": "/React-Todo-List-App/static/js/main.26b2cbab.chunk.js"
+  },
+  {
+    "revision": "b1b35313106c24e21197",
+    "url": "/React-Todo-List-App/static/js/runtime-main.cf1d5281.js"
+  }
+]);
